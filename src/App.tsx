3b47efb0e11ac94ac19c76716bea127223290d82
Temchuk <too060802@gmail.com>
2024-06-30 14:54:27 +0300

    hw2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React, {FC} from 'react';
 import './App.css';
-import {ICharacter} from "./models/ICharacter";
-import {simpsons} from "./data";
-import Character from "./components/character/Character";
-
-
 
 const App: FC = () => {
 
     return (
         <div>
-            {
-                simpsons.map((character: ICharacter) =>
-                    <Character character={character}>
-                        {character.info}
-                    </Character>)
-            }
+
         </div>
     );
 }
