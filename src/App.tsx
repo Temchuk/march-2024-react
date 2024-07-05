32fcb88cd640f3299a57665d25d5089cebbd0aaa
Temchuk <too060802@gmail.com>
2024-07-05 15:36:47 +0300

    con2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
 
 const App: FC = () => {
 
+
+    const [skipValue, setSkipValue] = useState<number>(0)
+
+    const [todos, setTodos] = useState<any[]>([])
+
+useEffect(() =>{
+    fetch('https://dummyjson.com/todo?skip=' + skipValue)
+        .then(value => value.json())
+        .then(todosResponse => {
+
+            setTodos(todosResponse.todos)
+        });
+    }, [skipValue]);
+
+
     return (
         <div>
-            {
 
-            }
+            {todos.map( value => <li>{value.todo}</li>)}
+
+            <button onClick={() => {
+               setSkipValue(skipValue - 30);
+            }}>prev
+            </button>
+
+            <button onClick={() => {
+                setSkipValue(skipValue + 30);
+            }}>next
+            </button>
         </div>
     );
 }
